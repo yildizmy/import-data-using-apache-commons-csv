e54cd7ae224dbdd101fcbb9707388fe7861809bb
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2022-07-26 10:48:53 +0300

    refactor: remove dots at the end of messsages

--- src/main/java/com/github/yildizmy/common/Constants.java
+++ src/main/java/com/github/yildizmy/common/Constants.java
@@ -10,12 +10,12 @@ public class Constants {
 
     // TODO: Move to message.properties file after applying i18n
     public static final String SUCCESSFULLY_UPLOADED="File uploaded successfully: ";
-    public static final String SUCCESSFULLY_DELETED="Successfully deleted.";
-    public static final String VALIDATION_ERROR="Validation error. Check 'errors' field for details.";
-    public static final String FAILED_TO_FIND_ELEMENT="Failed to find the requested element.";
-    public static final String INVALID_FILE_TYPE="Invalid file type.";
-    public static final String UNKNOWN_ERROR="Unknown error occurred.";
-    public static final String NO_RECORD="File does not contain any record.";
+    public static final String SUCCESSFULLY_DELETED="Successfully deleted";
+    public static final String VALIDATION_ERROR="Validation error. Check 'errors' field for details";
+    public static final String FAILED_TO_FIND_ELEMENT="Failed to find the requested element";
+    public static final String INVALID_FILE_TYPE="Invalid file type";
+    public static final String UNKNOWN_ERROR="Unknown error occurred";
+    public static final String NO_RECORD="File does not contain any record";
     public static final String FAILED_TO_PARSE="Failed to parse csv file: ";
-    public static final String NO_ITEM_FOUND="Item with given email not found.";
+    public static final String NO_ITEM_FOUND="Item with given email not found";
 }
